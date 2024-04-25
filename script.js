4ad71bcbecd4cf835c611ce822b5f8830075be6e
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 02:56:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
+	str = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 return str === str.split('').reverse().join('');
 }
 module.exports = palindrome
